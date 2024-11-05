b27d2c34b14eb331233db2414de8a013ca801b4f
vaggm <vaggosmitikas@gmail.com>
2024-11-05 22:49:59 +0200

    dependency updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Header from "./components/page-parts/Header";
 import Display from "./components/page-parts/Display";
 import "./App.css";
