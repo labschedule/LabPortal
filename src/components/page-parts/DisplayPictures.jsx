0a05bf8efeab3b7669dcacc7bbb4ef5a2e1c03f3
vaggm <vagosmitikas@gmail.com>
2024-11-18 17:24:31 +0200

    display qr for diplomas in CONSERT

--- src/components/page-parts/DisplayPictures.jsx
+++ src/components/page-parts/DisplayPictures.jsx
@@ -3,12 +3,16 @@ import React from "react";
 const DisplayPictures = ({ pictureName }) => {
 
   const source = `./images/${pictureName}`;
+  const QR = `./qr/${pictureName}`;
 
   return (
-    <div className="h-100 d-flex justify-content-center align-items-center">
-
-      <img src={source} alt="Lab Images" className="img-fluid" style={{ maxHeight: "75%" }}/>
-
+    <div className="h-100 d-flex justify-content-center align-items-center bg-light">
+      <div className="column w-50">
+        <img src={source} alt="Lab Images" className="img-fluid" style={{ maxHeight: "75%" }} />
+      </div>
+      <div className="column w-50">
+        <img src={QR} alt="Lab Images" className="img-fluid" style={{ maxHeight: "75%" }} />
+      </div>
     </div>
   );
 
