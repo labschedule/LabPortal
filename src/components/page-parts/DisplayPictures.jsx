28f5b42c764423163d9bc147a3969b111fd6e518
Evangelos Mitikas <105044468+VaggM@users.noreply.github.com>
2024-11-18 17:28:45 +0200

    Update DisplayPictures.jsx

--- src/components/page-parts/DisplayPictures.jsx
+++ src/components/page-parts/DisplayPictures.jsx
@@ -3,15 +3,15 @@ import React from "react";
 const DisplayPictures = ({ pictureName }) => {
 
   const source = `./images/${pictureName}`;
-  const QR = `./qr/${pictureName}`;
+  const source_qr = `./qr/${pictureName}`;
 
   return (
     <div className="h-100 d-flex justify-content-center align-items-center bg-light">
       <div className="column w-50">
-        <img src={source} alt="Lab Images" className="img-fluid" style={{ maxHeight: "75%" }} />
+        <img src={source} alt="Diploma" className="img-fluid" style={{ maxHeight: "75%" }} />
       </div>
       <div className="column w-50">
-        <img src={QR} alt="Lab Images" className="img-fluid" style={{ maxHeight: "75%" }} />
+        <img src={source_qr} alt="Diploma QR" className="img-fluid" style={{ maxHeight: "75%" }} />
       </div>
     </div>
   );
